f26424426322018a0d66778e676c55d8492629f6
Pauch1 <paulopuyot@gmail.com>
2024-08-14 15:31:10 +0800

    add new year

--- learn/hello/views.py
+++ learn/hello/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'hello/index.html')
 
 
 def greet(request, name):
     
-    return render (request, 'greet.html',{
+    return render (request, 'hello/greet.html',{
         "name": name.capitalize()
     })
